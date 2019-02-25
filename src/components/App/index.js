bafe9d86957e87411a887261a7419abb0a56399f
George-lee <rjsahdia3@gmail.com>
2019-02-25 09:47:29 +0900

    노트 추가, 삭제 기능
    로컬환경에서만 작동

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Header from '../Header'
 import List from '../List'
 import Note from '../Note'
+import { generateId } from '../../utils';
 
 class App extends React.Component {
 
@@ -34,6 +35,51 @@ class App extends React.Component {
     })
   }
 
+  handleEditNote = (type, evt) => {
+    // 새 notes 배열 만들기
+    const notes = [ ...this.state.notes]
+    
+    // 새 배열에서 현재 선택한(=activeNote) 노트 찾기
+    const note = notes.find((item) => item.id === this.state.activeId);
+
+    // 객체의 속성에 값 할당
+    note[type] = evt.target.value;
+
+    // notes에 새 배열을 할당하여 state변경
+    this.setState({
+      notes,
+    });
+  }
+
+  handleAddNote = () => {
+    const id = generateId(); // 랜덤 ID값 생성
+
+    this.setState({
+      notes: [
+        ...this.state.notes,
+        {
+          id,
+          title: '제목',
+          contents: '내용',
+        },
+      ],
+      activeId: id,
+    });
+  }
+
+  handleDeleteNote = () => {
+    // 지우려는 노트를 선택한 상태이니, 선택된 노트를 제외하고 새로운 배열을 생성
+    const notes = this.state.notes.filter((item) => {
+      return item.id !== this.state.activeId;
+    });
+
+    this.setState({
+      notes,
+      activeId: notes.length === 0 ? null : notes[0].id,
+    });
+  }
+
+
   render() {
     const { notes, activeId } = this.state;
 
@@ -42,15 +88,23 @@ class App extends React.Component {
 
     return(
       <div className="app">
-        <Header />
+        <Header 
+          onAddNote = {this.handleAddNote}
+          onDeleteNote = {this.handleDeleteNote}
+        />
         <div className="container">
           <List
             notes= {notes}
             activeId = {activeId}
             onListItemClick = {this.handleListItemClick}
           />
+          {/* 70번 라인은 notes배열이 0이상이고 사용자가 선택한 특정 노트가 있을 때 수행 */}
           {
-            notes.length > 0 && activeNote && <Note note = {activeNote} /> 
+            notes.length > 0 && activeNote && 
+            <Note
+              note = {activeNote} 
+              onEditNote = {this.handleEditNote}
+            /> 
           }
         </div>
       </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,14 +4,17 @@ import './index.css';
 
 class Header extends React.Component {
   render() {
+
+    const { onAddNote, onDeleteNote } = this.props;
+
     return(
       <div className="header">
         <div className="title">
           <span>George's 심플노트</span>
         </div>
         <div className="buttons">
-          <button>추가</button>
-          <button>삭제</button>
+          <button onClick = {onAddNote}>추가</button>
+          <button onClick = {onDeleteNote}>삭제</button>
         </div>
       </div>
     );
